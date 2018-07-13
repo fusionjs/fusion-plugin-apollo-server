c1149627d1716f52dbd734abdf311a9e65ca4883
Simon Robb <srobb@uber.com>
2018-07-13 13:12:17 -0400

    Provide apollo context to graphql server

--- src/server.js
+++ src/server.js
@@ -8,18 +8,23 @@
 
 import {createPlugin, type Middleware} from 'fusion-core';
 import {graphqlKoa} from 'apollo-server-koa';
-import {GraphQLSchemaToken} from 'fusion-apollo';
+import {GraphQLSchemaToken, ApolloContextToken} from 'fusion-apollo';
 import {ApolloServerEndpointToken} from './tokens';
 
 const plugin =
   __NODE__ &&
   createPlugin({
-    deps: {endpoint: ApolloServerEndpointToken, schema: GraphQLSchemaToken},
-    provides: ({schema}) =>
-      graphqlKoa(() => ({
+    deps: {
+      endpoint: ApolloServerEndpointToken,
+      schema: GraphQLSchemaToken,
+      context: ApolloContextToken.optional,
+    },
+    provides: ({schema, context}) =>
+      graphqlKoa(ctx => ({
         schema,
         tracing: true,
         cacheControl: true,
+        context: typeof context === 'function' ? context(ctx) : context,
       })),
     middleware: ({endpoint}, handler): Middleware => (ctx, next) =>
       ctx.path === endpoint ? handler(ctx) : next(),
