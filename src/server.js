de578677308d397866f2e4302a1a3406a0387cc3
Truong Hoang Dung <checkraiser11@gmail.com>
2018-08-28 23:23:41 +0700

    Support multipart file upload

--- src/server.js
+++ src/server.js
@@ -6,10 +6,16 @@
  *
  */
 
-import {createPlugin, type Middleware} from 'fusion-core';
+import {createPlugin, type Middleware, createToken} from 'fusion-core';
 import {graphqlKoa} from 'apollo-server-koa';
 import {GraphQLSchemaToken, ApolloContextToken} from 'fusion-apollo';
 import {ApolloServerEndpointToken} from './tokens';
+import fs from 'fs';
+import type {Token} from 'fusion-core';
+export const UploadDirToken: Token<string> = createToken(
+  'UploadDirToken'
+);
+import { apolloUploadKoa } from 'apollo-upload-server';
 
 const plugin =
   __NODE__ &&
@@ -18,8 +24,18 @@ const plugin =
       endpoint: ApolloServerEndpointToken,
       schema: GraphQLSchemaToken,
       apolloContext: ApolloContextToken.optional,
+      uploadDir: UploadDirToken,
     },
-    provides: ({schema, apolloContext}) =>
+    provides: ({schema, apolloContext, uploadDir = 'upload'}) => {
+      apolloUploadKoa({
+        uploadDir,
+      }),
+      async function(ctx, next) {
+        let { file } = ctx.request.body.variables;
+        if (ctx.request.is('multipart/form-data'))
+          fs.rename(file.path, uploadDir + file.name)
+        await next()
+      },
       graphqlKoa(ctx => ({
         schema,
         tracing: true,
@@ -28,7 +44,8 @@ const plugin =
           typeof apolloContext === 'function'
             ? apolloContext(ctx)
             : apolloContext,
-      })),
+      }))
+    },
     middleware: ({endpoint}, handler): Middleware => (ctx, next) =>
       ctx.path === endpoint ? handler(ctx) : next(),
   });
