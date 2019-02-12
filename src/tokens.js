90e47035968fe05a5439500c74a2f69f68dffc37
Dmitry Manannikov <email@slonoed.net>
2019-02-12 20:12:05 +0100

    Add `formatError` option to plugin config
    https://github.com/fusionjs/fusion-plugin-apollo-server/pull/66
    
    Co-authored-by: Dmitry Manannikov <slonoed@uber.com>

--- src/tokens.js
+++ src/tokens.js
@@ -4,3 +4,7 @@ import {createToken, type Token} from 'fusion-core';
 export const ApolloServerEndpointToken: Token<string> = createToken(
   'ApolloServerEndpointToken'
 );
+
+export const ApolloServerFormatFunctionToken: Token<Function> = createToken(
+  'ApolloServerFormatFunctionToken'
+);
